6b49081c3c0ed301b412d8282db374f221ed4a05
vladimircaraseni <vcaraseni@gmail.com>
2024-06-30 18:08:27 +0300

    comment existed api route which was used just for testing

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('status', static fn() => new JsonResponse(['status' => 'ok']));
+//Route::get('status', static fn() => new JsonResponse(['status' => 'ok']));
